f750a74308cfa3cb28927a5ac5bec0f66a1c0128
Grzegorz Bokota <g.bokota@cent.uw.edu.pl>
2019-11-26 00:26:25 +0100

    bases for library compilation

---
+++ build_utils/build_libraries.sh
@@ -0,0 +1,151 @@
+#!/usr/bin/env bash
+set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PATH="/usr/local/opt/gettext/bin:$PATH"
+build_dir=${DIR}/libs_build
+download_dir=${DIR}/libs_src
+PATH=${build_dir}/bin:${PATH}
+LD_LIBRARY_PATH=${build_dir}/lib:${LD_LIBRARY_PATH}
+# export CPLUS_INCLUDE_PATH=:${build_dir}/include:${CPLUS_INCLUDE_PATH}
+
+if [ -x "$(command -v cmake3)" ]; then
+  CMAKE3=cmake3
+else
+  CMAKE3=cmake
+fi
+
+ls "${download_dir}"
+
+mkdir -p "${build_dir}"
+
+# echo "Build openssl"
+# cd "${download_dir}/openssl"
+# SYSTEM=$(uname -s) ./config --prefix="${build_dir}"
+# make
+# make test
+# make install
+
+echo "build swig"
+cd "${download_dir}/swig-3.0.10" || exit 1
+./configure --prefix="${build_dir}"
+make
+make install
+
+echo "Build jxrlib"
+cd "${download_dir}/jxrlib" || exit 1
+git stash
+git apply "${DIR}/jxrlib.patch" # for macos build
+PREFIX="${build_dir}" DIR_INSTALL="${build_dir}" make swig
+PREFIX="${build_dir}" DIR_INSTALL="${build_dir}" make
+PREFIX="${build_dir}" DIR_INSTALL="${build_dir}" make install
+cd "${build_dir}/lib" || exit 1
+if [ "$(uname)" == "Darwin" ]; then
+  ln -s libjxrglue.dylib libjxrglue.dylib.0 || true
+  ln -s libjpegxr.dylib libjpegxr.dylib.0 || true
+else
+  ln -s libjxrglue.so libjxrglue.so.0 || true
+  ln -s libjpegxr.so libjpegxr.so.0 || true
+fi
+
+echo "Build libpng"
+cd "${download_dir}/libpng" || exit 1
+mkdir -p build
+cd build || exit 1
+${CMAKE3} -DCMAKE_INSTALL_PREFIX="${build_dir}" ..
+make install
+
+
+echo "Build bzip2"
+cd "${download_dir}/bzip2" || exit 1
+git stash
+git apply "${DIR}/bzip2.patch" # for macos build
+make -f Makefile-libbz2_so
+make install PREFIX="${build_dir}"
+cp libbz2.so.1.0.8 "${build_dir}"/lib
+cp libbz2.so.1.0 "${build_dir}"/lib
+
+echo "Build c-blosc"
+cd "${download_dir}/c-blosc" || exit 1
+mkdir -p build
+cd build || exit 1
+cmake -DCMAKE_INSTALL_PREFIX="${build_dir}" ..
+cmake --build .
+# ctest
+cmake --build . --target install
+
+echo "Build charls"
+cd "${download_dir}/charls" || exit 1
+git stash
+git apply "${DIR}/charls.patch"
+mkdir -p build
+cd build || exit 1
+cmake -DCMAKE_INSTALL_PREFIX="${build_dir}" -DBUILD_SHARED_LIBS=1 -DCMAKE_BUILD_TYPE=Release ..
+make install
+
+echo "Build libjpeg-turbo"
+cd "${download_dir}/libjpeg-turbo" || exit 1
+mkdir -p build
+cd build || exit 1
+cmake -G"Unix Makefiles" -DCMAKE_INSTALL_PREFIX="${build_dir}" -DWITH_JPEG8=1 -WITH_12BIT=1 ..
+make install
+
+echo "Build liblzf"
+cd "${download_dir}/liblzf" || exit 1
+sh ./configure --prefix="${build_dir}"
+make
+make install
+
+echo "Build libwebp"
+cd "${download_dir}/libwebp" || exit 1
+sh ./autogen.sh
+sh ./configure --prefix="${build_dir}"
+make
+make install
+cd "${build_dir}/lib" || exit 1
+ln -s libwebp.so.7 libwebp.so.6
+
+echo "Build Little-CMS"
+cd "${download_dir}/Little-CMS" || exit 1
+sh ./configure --prefix="${build_dir}"
+make
+make install
+
+echo "Build lz4"
+cd "${download_dir}/lz4" || exit 1
+make
+PREFIX="${build_dir}" make install
+
+echo "Build openjpeg"
+cd "${download_dir}/openjpeg" || exit 1
+mkdir -p build
+cd build || exit 1
+cmake -DCMAKE_INSTALL_PREFIX="${build_dir}" ..
+make install
+
+echo "Build xz"
+cd "${download_dir}/xz" || exit 1
+sh ./autogen.sh
+sh ./configure --prefix="${build_dir}"
+make
+make check
+make install
+make installcheck
+
+echo "Build zfp"
+cd "${download_dir}/zfp" || exit 1
+mkdir -p build
+cd build || exit 1
+${CMAKE3} -DCMAKE_INSTALL_PREFIX="${build_dir}" ..
+make install
+
+echo "Build zlib"
+cd "${download_dir}/zlib" || exit 1
+mkdir -p build
+cd build || exit 1
+cmake -DCMAKE_INSTALL_PREFIX="${build_dir}" ..
+make install
+
+echo "Build zstd"
+cd "${download_dir}/zstd" || exit 1
+make
+PREFIX="${build_dir}" make install
--- build_utils/download_libraries.sh
+++ build_utils/download_libraries.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/env bash
-mkdir -p libs_src
-git clone --depth 1 --branch v1.2.11 https://github.com/madler/zlib libs_src/zlib
-git clone --depth 1 --branch v1.9.2 https://github.com/lz4/lz4 libs_src/lz4
-git clone --depth 1 --branch v1.4.4 https://github.com/facebook/zstd libs_src/zstd
-git clone --depth 1 --branch v1.17.0 https://github.com/Blosc/c-blosc libs_src/c-blosc
-git clone --depth 1 --branch bzip2-1.0.8 git://sourceware.org/git/bzip2.git libs_src/bzip2
-git clone --depth 1 --branch v5.2.4 https://github.com/xz-mirror/xz libs_src/xz
-git clone --depth 1 --branch v1.6.37 https://github.com/glennrp/libpng libs_src/libpng
-git clone --depth 1 --branch v1.0.3 https://github.com/webmproject/libwebp libs_src/libwebp
-git clone --depth 1 --branch 2.0.3 https://github.com/libjpeg-turbo/libjpeg-turbo libs_src/libjpeg-turbo
-git clone --depth 1 --branch 2.0.0 https://github.com/team-charls/charls libs_src/charls
-git clone --depth 1 --branch v2.3.1 https://github.com/uclouvain/openjpeg libs_src/openjpeg
-git clone --depth 1 --branch v0.2.1 https://github.com/glencoesoftware/jxrlib libs_src/jxrlib
-git clone --depth 1 --branch 0.5.5 https://github.com/LLNL/zfp libs_src/zfp
-git clone --depth 1 --branch lcms2.9 https://github.com/mm2/Little-CMS libs_src/Little-CMS
-wget http://dist.schmorp.de/liblzf/liblzf-3.6.tar.gz
-tar zxvf  liblzf-3.6.tar.gz -C libs_src
-mv libs_src/liblzf-3.6 libs_src/liblzf
+set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+download_dir=${DIR}/libs_src
+mkdir -p "${download_dir}"
+# git clone --depth 1 --branch OpenSSL_1_1_1d https://github.com/openssl/openssl ${download_dir}/openssl
+git clone --depth 1 --branch v1.2.11 https://github.com/madler/zlib "${download_dir}/zlib"
+git clone --depth 1 --branch v1.9.2 https://github.com/lz4/lz4 "${download_dir}/lz4"
+git clone --depth 1 --branch v1.4.4 https://github.com/facebook/zstd "${download_dir}/zstd"
+git clone --depth 1 --branch v1.17.0 https://github.com/Blosc/c-blosc "${download_dir}/c-blosc"
+git clone --depth 1 --branch bzip2-1.0.8 git://sourceware.org/git/bzip2.git "${download_dir}/bzip2"
+git clone --depth 1 --branch v5.2.4 https://github.com/xz-mirror/xz "${download_dir}/xz"
+git clone --depth 1 --branch v1.6.37 https://github.com/glennrp/libpng "${download_dir}/libpng"
+git clone --depth 1 --branch v1.0.3 https://github.com/webmproject/libwebp "${download_dir}/libwebp"
+git clone --depth 1 --branch 2.0.3 https://github.com/libjpeg-turbo/libjpeg-turbo "${download_dir}/libjpeg-turbo"
+git clone --depth 1 --branch 2.0.0 https://github.com/team-charls/charls "${download_dir}/charls"
+git clone --depth 1 --branch v2.3.1 https://github.com/uclouvain/openjpeg "${download_dir}/openjpeg"
+git clone --depth 1 --branch v0.2.1 https://github.com/glencoesoftware/jxrlib "${download_dir}/jxrlib"
+git clone --depth 1 --branch 0.5.5 https://github.com/LLNL/zfp "${download_dir}/zfp"
+git clone --depth 1 --branch lcms2.9 https://github.com/mm2/Little-CMS "${download_dir}/Little-CMS"
+wget -q http://dist.schmorp.de/liblzf/liblzf-3.6.tar.gz
+tar zxvf  liblzf-3.6.tar.gz -C "${download_dir}"
+mv "${download_dir}/liblzf-3.6" "${download_dir}/liblzf"
+
+wget -q 'https://sourceforge.net/projects/swig/files/swig/swig-3.0.10/swig-3.0.10.tar.gz'
+tar zxvf  swig-3.0.10.tar.gz -C "${download_dir}"
