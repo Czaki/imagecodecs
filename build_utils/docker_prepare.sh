f579eba5d57cb8da55f1f24bd8fafdfb02320a59
Grzegorz Bokota <bokota+github@gmail.com>
2020-01-17 08:31:57 +0100

    add manylinux 2014 builds
    move cmake build to previous steep in 2010 dockerfiles to speedup rebuild

--- build_utils/docker_prepare.sh
+++ build_utils/docker_prepare.sh
@@ -3,10 +3,4 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 yum install -y pcre pcre-devel openssl-devel java-1.8.0-openjdk-devel wget libtool cmake3 snappy-devel
 
-wget https://github.com/Kitware/CMake/releases/download/v3.16.0/cmake-3.16.0.tar.gz
-tar zxvf  cmake-3.16.0.tar.gz
-cd cmake-3.16.0
-./bootstrap && make && make install
-make clean
-
 bash "${DIR}/gettext_install.sh"
