df8427d11063620e5d5f3e64076adb5aacde9c6e
Grzegorz Bokota <bokota+github@gmail.com>
2020-05-15 01:32:58 +0200

    timeout fixes

--- build_utils/install_cmake.sh
+++ build_utils/install_cmake.sh
@@ -3,10 +3,13 @@ set -e
 
 yum install -y wget openssl-devel
 
-wget https://github.com/Kitware/CMake/releases/download/v3.16.0/cmake-3.16.0.tar.gz
-tar zxvf  cmake-3.16.0.tar.gz
+wget -q https://github.com/Kitware/CMake/releases/download/v3.16.0/cmake-3.16.0.tar.gz
+tar zxf  cmake-3.16.0.tar.gz
 cd cmake-3.16.0
-./bootstrap > /dev/null
+echo "Bootstrap cmake"
+./bootstrap
+echo "Build cmake"
 make -s -j $(nproc)
+echo "install cmake"
 make install
 make clean
