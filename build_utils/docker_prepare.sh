71a2a961cd67c30ed3964ea343cbd429253b3660
Grzegorz Bokota <bokota+github@gmail.com>
2020-05-15 02:02:10 +0200

    remove cmake3 instal from yum

--- build_utils/docker_prepare.sh
+++ build_utils/docker_prepare.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-yum install -y pcre pcre-devel openssl-devel java-1.8.0-openjdk-devel wget libtool cmake3 snappy-devel
+yum install -y pcre pcre-devel openssl-devel java-1.8.0-openjdk-devel wget libtool snappy-devel
 
 bash "${DIR}/gettext_install.sh"
